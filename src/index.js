f2df79accd40a3ba4e03520a6f757278689251bc
Tran Vi <vitren63666@gmail.com>
2025-04-26 19:33:24 +0700

    config(cors): add url dev frontend

--- src/index.js
+++ src/index.js
@@ -15,9 +15,22 @@ const app = express();
 const httpServer = createServer(app);
 const port = process.env.PORT || 4000;
 
+const allowedOrigins = [
+    process.env.FRONTEND_URL_PROD,
+    process.env.FRONTEND_URL_DEV,
+];
+
 // CORS middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: [
         'Content-Type', 
